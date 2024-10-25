867c7dc56187cd9edd77ca21245446c9533008ac
vnglc00 <vpallerano@uc.cl>
2024-10-24 21:46:44 -0300

    Cambio página sin js

--- mybf.js
+++
@@ -1,7 +0,0 @@
-let currentSection = 0;
-
-function nextSection(section) {
-    const container = document.querySelector('.container');
-    currentSection = section;
-    container.style.transform = `translateX(-${currentSection * 100}vw)`;
-}
